c31fc3adc16ca051b4b9757d4e94df869df17466
hericlibong <heric.afrimages@gmail.com>
2025-11-11 17:10:56 +0100

    chore(quali_duels): untrack unfinished module (kept locally for drafts)

--- projects/quali_duels/main.py
+++
@@ -1,6 +0,0 @@
-from qualifying_duel_builder import QualifyingDuelBuilder
-
-if __name__ == "__main__":
-    builder = QualifyingDuelBuilder(season=2025)
-    builder.fetch_duels_for_all_rounds()
-    builder.export_csv()
